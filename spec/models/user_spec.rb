d533f10ecdfc100b4e6f1ea5f45026de6a6eedd8
BJK <bernie@thecoderie.com>
2013-10-29 10:16:17 -0400

    fixed syntax error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  	before(:each) do
-      @user = User.new(first_name:"Fred", last_name: "Stevens", email:'fred@gmail.com')
+  before(:each) do
+    @user = User.new(first_name:"Fred", last_name: "Stevens", email:'fred@gmail.com')
+  end
 
-    it "has an email" do
-      @user.email.should be_present
-    end
+  it "has an email" do
+    @user.email.should be_present
+  end
 
-    it "has a first name" do
-      @user.first_name.should be_present
-    end
+  it "has a first name" do
+    @user.first_name.should be_present
+  end
 
-    it "has a last name" do
-      @user.last_name.should be_present
-    end
+  it "has a last name" do
+    @user.last_name.should
   end
 end
