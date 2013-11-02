973322b6e2a2448c26240503ca854cf7d899a0b0
Johnny <johnnyin2a@gmail.com>
2013-11-02 18:17:50 -0400

    Restored the file to its final state.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,4 +21,14 @@ describe User do
     @user.fullname.should == "Fred Stevens"
   end
 
+  context "with a link" do
+    before(:each) do
+      @user = FactoryGirl.create(:user_with_link)
+    end
+    
+    it "has a link" do
+      @user.links.count.should == 1
+    end
+  end
+
 end
